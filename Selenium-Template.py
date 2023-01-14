976ca73ac06ee3dcc6d87ece01a1cfcd702e73ae
PyNAABO <86821481+PyNAABO@users.noreply.github.com>
2023-01-14 22:27:04 +0530

    264wwrt

--- Selenium-Template.py
+++ Selenium-Template.py
@@ -37,4 +37,6 @@ driver = webdriver.Chrome(options = chrome_options)
 driver.get('http://github.com')
 print(driver.title)
 with open('./GitHub_Action_Results.txt', 'w') as f:
-    f.write(f"This was written with a GitHub action {driver.title}", "-", datetime.now())
+    string = f"This was written with a GitHub action {driver.title}", "-", datetime.now()
+    f.write(string)
+
